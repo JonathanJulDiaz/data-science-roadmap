565bd482e8701adb94f5c49d0afce885d1d9901c
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-17 00:40:32 -0500

    We improved the car game

--- programming-with-Python/Programs/car_game.py
+++ programming-with-Python/Programs/car_game.py
@@ -5,13 +5,23 @@
 # we will put '> ' in front of the input
 command = input("> ").lower() # lower() is used to convert the input to lowercase, so it is not case sensitive
 
+is_car_started = False # Variable to check if the car is started
+
 # We must write the code inside a loop, so that the user can continue to enter commands
 while command != 'quit': # Condition to check if the user wants to quit the game
 
     if command == 'start':
-        print("Car started... Ready to go")
-    elif command == 'stop':
-        print("Car stopped")
+        if is_car_started: # If the car is already started
+            print("Car is already started")
+        else:
+            print("Car started... Ready to go")
+            is_car_started = True
+    elif command == 'stop' :
+        if not is_car_started: # If the car is not started
+            print("Car is already stopped")
+        else:
+            print("Car stopped")
+            is_car_started = False
     elif command == 'help': # If the user enters 'help'
         print("""
         start - to start the car
