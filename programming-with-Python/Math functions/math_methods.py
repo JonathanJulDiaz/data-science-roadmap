dc1be0283e66ef445d422e3600b43a63866e85a0
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-16 23:28:27 -0500

    Introduce to math module

---
+++ programming-with-Python/Math functions/math_methods.py
@@ -0,0 +1,19 @@
+# Python have differents math functions, for example the function round()
+# This function is used to round a number to the nearest integer
+print(round(2.7))
+
+# Another example is the function abs()
+# This function is used to get the absolute value of a number
+print(abs(-2.7))
+
+# But Python have a more powerfiul math module, which is the math module
+# This module have many useful functions, like sqrt(), pow(), ceil(), floor()
+# To use it, we need to import it
+import math
+
+print(math.sqrt(16)) # This will print the square root of 16
+print(math.pow(2, 3)) # This will print 2 to the power of 3
+print(math.ceil(2.7)) # This will print the ceiling of 2.7 which is 3
+print(math.floor(2.7)) # This will print the floor of 2.7 which is 2
+
+# To know more about the math module, you can search it in the python documentation
