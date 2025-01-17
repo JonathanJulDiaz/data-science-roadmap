3f683711b8e729e5247cfe2abc163fb1d04416ad
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-16 22:52:11 -0500

    Add the formatted strings to know how they works

---
+++ programming-with-Python/Strings/formatted_strings.py
@@ -0,0 +1,9 @@
+# Formatted Strings are strings that have placeholders for variables
+# We can use the f-string to format a string
+name = 'John'
+age = 30
+
+# We use {} to place the variable inside the string
+message = f'Hello, {name}. You are {age} years old.'
+
+print(message) # This will print: Hello, John. You are 30 years old.
