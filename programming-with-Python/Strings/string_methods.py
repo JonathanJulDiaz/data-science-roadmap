cc93c944f0e9c54561cd490beeb504b9147da100
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-16 23:02:21 -0500

    Show how to use the String methods using the dot

---
+++ programming-with-Python/Strings/string_methods.py
@@ -0,0 +1,16 @@
+# Python have many useful functions that we can use on strings
+course = 'Python for Beginners'
+
+# For example, len() returns the length of the object inside it,
+# in this case course, which is 20
+print(len(course))
+
+# But the String also have methods that we can use
+# For example, upper() returns the string in uppercase
+print(course.upper())
+
+# Note that to use a method from the String course, we
+# have to use the . operator to access it
+print(course.lower()) # This will print the string in lowercase
+
+# There are many methods from the Strings, you can search them in the python documentation
