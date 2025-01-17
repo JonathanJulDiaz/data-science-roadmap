51dbc77c3eb27d9e8a9b830a097d0d48f0a12ec9
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-16 21:10:20 -0500

    Add the input.py to understand how the function works

---
+++ programming-with-Python/input.py
@@ -0,0 +1,9 @@
+# The function input() is used to get input from the user
+
+# Inside the input() function, we pass a message to the user as a parameter
+name = input("Enter your name here: ")
+
+# Then, we can concatenate the user input with a message
+# using the + operator, which is the concatenation operator
+# usually used to combine short and simple strings
+print("Hello, " + name)
