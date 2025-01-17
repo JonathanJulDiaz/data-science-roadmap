44a27bbd8ed0c77a3766ccddcfb7556de701f920
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-17 17:09:29 -0500

    Add the list methods

---
+++ programming-with-Python/Lists/list_methods.py
@@ -0,0 +1,19 @@
+# The lists have useful methods like append(), insert(), remove(), pop(), clear(), index(), count(), sort(), reverse()
+# and many more
+
+numbers = [1, 2, 3, 4, 5]
+
+numbers.append(6) # This will add the number 6 to the end of the list
+print(numbers) # This will print the list with the number 6 at the end
+
+numbers.insert(0, 0) # This will add the number 0 to the beginning of the list  
+print(numbers) # This will print the list with the number 0 at the beginning
+
+numbers.remove(3) # This will remove the number 3 from the list
+print(numbers) # This will print the list without the number 3
+
+numbers.pop() # This will remove the last number from the list
+print(numbers) # This will print the list without the last number
+
+numbers.clear() # This will clear the list
+print(numbers) # This will print an empty list
