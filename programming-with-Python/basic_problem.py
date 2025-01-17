92b32e8add6c37fb5961802fd12a19213593c525
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-16 21:02:58 -0500

    Add an solution for a basic problem using variables to store data

---
+++ programming-with-Python/basic_problem.py
@@ -0,0 +1,20 @@
+# The patient, John Doe, comes into the clinic complaining of feeling very hot,
+# experiencing chills, and having a high fever. The nurse takes his vital signs
+# and records the necessary information. John is also asked whether he has
+# social security for insurance purposes.
+
+# Write a Python program that uses variables to store the patient's name,
+# temperature, and insurance status. The program should then print a report
+# that includes the patient's name, age, temperature(in celsius), and insurance status.
+
+# Solution
+
+name = "John Doe"
+age = 30
+temperature = 39.2
+has_insurance = True
+
+print("Patient Name:", name)
+print("Age:", age)
+print("Temperature(Celsius):", temperature)
+print("Has Insurance:", has_insurance)
