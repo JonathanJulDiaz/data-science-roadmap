079e798f82bfded9bc958ebd13be789b7e97f780
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-16 20:55:47 -0500

    Types of basic variables

--- programming-with-Python/variables.py
+++ programming-with-Python/variables.py
@@ -5,3 +5,14 @@ price = 10
 
 # Print the value of the price variable
 print(price)
+
+
+# Types of basic variables
+# Strings: Text or characters
+name = "John"
+# Integers: Whole numbers
+age = 30
+# Floats: Decimal numbers
+rating = 3.5
+# Booleans: Can be True or False
+is_student = True
