14dc6d3c12f42bd0c2733a0f8cc74cbecf614d9b
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-16 23:14:24 -0500

    Introduce the python operators

---
+++ programming-with-Python/Operations/operators.py
@@ -0,0 +1,75 @@
+# In python we can use differents operators
+# Operators are used to perform operations on variables and values,
+# to compare values and return a boolean result,
+# to assign values to variables,
+# and are used to control the flow of a program
+
+# Arithmetic Operators: This operators are used to perform arithmetic operations
+# + Addition
+print(f'Addition: {2 + 3}')
+# - Subtraction
+print(f'Subtraction: {2 - 3}')
+# * Multiplication
+print(f'Multiplication: {2 * 3}')
+# / Division
+print(f'Division: {2 / 3}')
+# % Modulus
+print(f'Modulus: {9 % 3}')
+# ** Exponentiation
+print(f'Exponentiation: {2 ** 3}')
+# // Floor Division
+print(f'Floor Division: {2 // 3}')
+
+print()
+
+# Assignment Operators: This operators are used to assign values to variables
+# = Assignment
+number = 10
+print(f'Number: {number}')
+# += Addition Assignment
+number += 5
+print(f'Added 5: {number}')
+# -= Subtraction Assignment
+number -= 5
+print(f'Subtracted 5: {number}')
+# *= Multiplication Assignment
+number *= 5
+print(f'Multiplied 5: {number}')
+# /= Division Assignment
+number /= 5
+print(f'Divided 5: {number}')
+# %= Modulus Assignment
+number %= 5
+print(f'Modulus 5: {number}')
+# **= Exponentiation Assignment
+number **= 5
+print(f'Exponentiation 5: {number}')
+# //= Floor Division Assignment(Divide and round the result down to the nearest whole number)
+number //= 5
+print(f'Floor Division 5: {number}')
+
+print()
+
+# Comparison Operators: This operators are used to compare values and return a boolean result
+# > Greater than
+print(f'2 > 3: {2 > 3}')
+# < Less than
+print(f'2 < 3: {2 < 3}')
+# >= Greater than or equal to
+print(f'2 >= 3: {2 >= 3}')
+# <= Less than or equal to
+print(f'2 <= 3: {2 <= 3}')
+# == Equal to
+print(f'2 == 3: {2 == 3}')
+# != Not equal to
+print(f'2 != 3: {2 != 3}')
+
+print()
+
+# Logical Operators: This operators are used to associate boolean values and return a boolean result
+# and
+print(True and False)
+# or(This is an inclusive or)
+print(True or False)
+# not
+print(not True)
