3ce70ce6880e60631f605f2a02064937c9530413
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-17 21:31:04 -0500

    Add an introduction about the return keyword in functions

---
+++ programming-with-Python/Functions/return.py
@@ -0,0 +1,8 @@
+# In the function we can use the keyword return to return a value
+# This value will be returned to the caller of the function
+
+# For example, if we want to return the sum of two numbers
+def sum(a, b):
+    return a + b # This will sum the two numbers and return the result
+
+print(f"The sum of 2 and 3 is: {sum(2, 3)}")
