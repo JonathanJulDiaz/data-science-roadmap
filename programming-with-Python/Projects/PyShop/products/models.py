64eb331f8272cc53d447ecd46d34fdb5ae398c7d
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-21 22:20:35 -0500

    Create a model in Django

--- programming-with-Python/Projects/PyShop/products/models.py
+++ programming-with-Python/Projects/PyShop/products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name : str = models.CharField(max_length=255)
+    price : float = models.DecimalField(max_digits=10, decimal_places=2)
+    stock : int = models.IntegerField(default=0)
+    image_url : str = models.CharField(max_length=2083)
