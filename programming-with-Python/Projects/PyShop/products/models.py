5b2ef33d470e975039e1b7498732f7141abe6efd
Jonathan Julio <yamidtjulio@hotmail.com>
2025-01-21 22:44:10 -0500

    Add a new class Offer

--- programming-with-Python/Projects/PyShop/products/models.py
+++ programming-with-Python/Projects/PyShop/products/models.py
@@ -6,3 +6,9 @@ class Product(models.Model):
     price : float = models.DecimalField(max_digits=10, decimal_places=2)
     stock : int = models.IntegerField(default=0)
     image_url : str = models.CharField(max_length=2083)
+
+
+class Offer(models.Model):
+    code : str = models.CharField(max_length=100)
+    description : str = models.CharField(max_length=50)
+    discount : float = models.DecimalField(max_digits=10, decimal_places=2)
